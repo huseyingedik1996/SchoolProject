154ce6326dfde7fca2124488a5c60aa7c17418d6
Halil Kürel <kurelhalil34@gmail.com>
2024-02-11 22:15:39 +0300

    ClassesService-11.02

--- School.Business/Services/ClassService.cs
+++ School.Business/Services/ClassService.cs
@@ -4,8 +4,10 @@ using School.Business.Services.ServiceInterfaces;
 using School.Business.Validations.ClassesValidations;
 using School.Business.Validations.StudentValidations;
 using School.Common.Response;
+using School.DataAccess.Models;
 using School.DataAccess.Repositories;
 using School.Dto.Dtos.ClassDtos;
+using School.Dto.Dtos.StudentDto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,29 +31,78 @@ namespace School.Business.Services
             _updateValidator = updateValidator;
         }
 
-        public Task<IResponse<ClassCreateDto>> Create(ClassCreateDto createStudent)
+        public async Task<IResponse<ClassCreateDto>> Create(ClassCreateDto createClass)
         {
-            throw new NotImplementedException();
+            var validationResult = _createValidator.Validate((IValidationContext)createClass);
+            if (validationResult.IsValid)
+            {
+                await _uow.GetRepositores<Classes>().Create(_mapper.Map<Classes>(createClass));
+                await _uow.SaveChangesAsync();
+                return new ResponseT<ClassCreateDto>(ResponseType.Success, createClass);
+            }
+            else
+            {
+                List<CustomValidationError> errors = new();
+                foreach (var error in validationResult.Errors)
+                {
+                    errors.Add(new()
+                    {
+                        ErrorMessage = error.ErrorMessage,
+                        PropertyName = error.PropertyName
+                    });
+                }
+                return new ResponseT<ClassCreateDto>(ResponseType.ValidationError, createClass, errors);
+            }
         }
 
-        public Task<IResponse<List<ClassListDto>>> GetAll()
+        public async Task<IResponse<List<ClassListDto>>> GetAll()
         {
-            throw new NotImplementedException();
+            var data = _mapper.Map<List<ClassListDto>>(await _uow.GetRepositores<Classes>().GetAll());
+            return new ResponseT<List<ClassListDto>>(ResponseType.Success, data);
         }
 
-        public Task<IResponse<ClassListDto>> GetById(int id)
+        public async Task<IResponse<ClassListDto>> GetById(int id)
         {
-            throw new NotImplementedException();
+            var data = _mapper.Map<ClassListDto>(await _uow.GetRepositores<Classes>().GetByFilter(x => x.Id == id));
+            return new ResponseT<ClassListDto>(ResponseType.Success, data);
         }
 
-        public Task<IResponse> Remove(int id)
+        public async Task<IResponse> Remove(int id)
         {
-            throw new NotImplementedException();
+            var removedEntry = await _uow.GetRepositores<Classes>().GetByFilter(x => x.Id == id);
+            if (removedEntry != null)
+            {
+                _uow.GetRepositores<Classes>().Delete(removedEntry);
+                await _uow.SaveChangesAsync();
+                return new ResponseT<bool>(ResponseType.Success, removedEntry != null);
+            }
+            return new ResponseT<bool>(ResponseType.NotFound, $"{id} not found.");
         }
 
-        public Task<IResponse<List<ClassUpdateDto>>> UpdateDtos(ClassUpdateDto updateStudent)
+        public async Task<IResponse<List<ClassUpdateDto>>> UpdateDtos(ClassUpdateDto updateClass)
         {
-            throw new NotImplementedException();
+            var validationResult = _updateValidator.Validate((IValidationContext)updateClass);
+            if (!validationResult.IsValid)
+            {
+                List<CustomValidationError> errors = validationResult.Errors.Select(error => new CustomValidationError
+                {
+                    ErrorMessage = error.ErrorMessage,
+                    PropertyName = error.PropertyName
+                }).ToList();
+                return new ResponseT<List<ClassUpdateDto>>(ResponseType.NotFound, "Student not found.");
+            }
+
+            var updatedEntity = await _uow.GetRepositores<ClassUpdateDto>().GetById(updateClass.Id);
+            if (updatedEntity != null)
+            {
+                _uow.GetRepositores<ClassUpdateDto>().Update(_mapper.Map<ClassUpdateDto>(updateClass), updatedEntity);
+                await _uow.SaveChangesAsync();
+                return new ResponseT<List<ClassUpdateDto>>(ResponseType.Success, "Student updated successfully.");
+            }
+            else
+            {
+                return new ResponseT<List<ClassUpdateDto>>(ResponseType.NotFound, "Student not found.");
+            }
         }
     }
 }
