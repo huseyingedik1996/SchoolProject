14484bd7d05b5937b11f4cb4d0033f6af34d690d
Halil Kürel <kurelhalil34@gmail.com>
2024-02-11 14:31:08 +0300

    Dto-11.02

---
+++ School.Dto/Dtos/ClassDtos/ClassCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Dto.Dtos.ClassDtos
+{
+    public class ClassCreateDto
+    {
+
+        public string ClassName { get; set; }
+    }
+}
