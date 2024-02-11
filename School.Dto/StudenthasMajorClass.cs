d2436763d0741e5a6a47baff5f83415e4e569371
Halil Kürel <kurelhalil34@gmail.com>
2024-02-11 14:56:46 +0300

    DTO-11.02-4

---
+++ School.Dto/StudenthasMajorClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.DataAccess.Models
+{
+    public class StudenthasMajorClass
+    {
+        public int Id { get; set; }
+        public int StudentsId { get; set; }
+        public int MajorhasClassesId { get; set; }
+
+        public MajorhasClass MajorhasClasses { get; set; }
+        public Students Students { get; set; }
+    }
+}
